e211089afe860ce000a5ac3e10efeb971824b370
Fatih <fatihcihan666@gmail.com>
2023-01-31 20:30:05 +0300

    Gereksiz alanlar temizlendi

--- OOP.AddAClass/Program.cs
+++ OOP.AddAClass/Program.cs
@@ -7,32 +7,10 @@ namespace OOP.AddAClass
     {
         static void Main(string[] args)
         {
-            var list = new List<Employee>()
-            {
-                new Employee {FirstName="Fatih", LastName="Cihan", Age=30},
-                new Employee {FirstName="Mehmet", LastName="Özer", Age=20},
-                new Employee {FirstName="Merve", LastName="Aslan", Age=25},
-            };
+            var employee = new Employee();
+            employee.Add(new Employee { FirstName = "Fatih", LastName = "Cihan", Age = 30 });
 
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.ReadLine();
-
-            //Employee employee = new Employee()
-            //{
-            //    FirstName = "Fatih",
-            //    LastName = "Cihan",
-            //    Age = 30,
-            //};
-            //Employee employee2 = new Employee("Mehmet", "Özer", 30);
-            //Employee employee3 = new Employee("Merve", "Aslan", 20);
-
-            //list.Add(employee);
-            //list.Add(employee2);
-            //list.Add(employee3);
+            Console.ReadLine();            
         }
     }
 }
