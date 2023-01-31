ac3ddd92bed3d4b5041993a97184b3298db1035f
Fatih <fatihcihan666@gmail.com>
2023-01-31 19:47:17 +0300

    Class tanimlama, access modifier, property, encapsulation, constructor metot gibi konulara giris yapildi

--- OOP.AddAClass/Employee.cs
+++ OOP.AddAClass/Employee.cs
@@ -9,12 +9,42 @@ namespace OOP.AddAClass
     internal class Employee
     {
         private string _firstName;
-
+        private string _lastName;
+        private List<Employee> _list;
         public string FirstName     // encapsulation
         {
             set { _firstName = value; }
             get { return _firstName; }
         }
+        public string LastName
+        {
+            // expression-bodied property accessors
+            get => _lastName;
+            set => _lastName = value;
+        }
+        public int Age { get; set; }     // auto-implemented property  ->  custom bir control yapilmayacaksa...
+
+        public Employee()   // default olarak zaten var, nesneden instance alinca calisan ilk metot.
+        {
+            _list = new List<Employee>();
+            // Console.WriteLine("Constructor method worked");
+        }
+
+        // Overloading
+        public Employee(string firstName, string lastName, int age) // this -> Employee classini temsil eder.
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            _list = new List<Employee>();
+        }
+
+        public override string ToString()       // Override (object (base) classtan gelen tostring metodunu eziyoruz)
+        {
+            return $"{FirstName,-20} {LastName,-15} {Age,5}";
+        }
+
+       
 
     }
 }
--- OOP.AddAClass/Program.cs
+++ OOP.AddAClass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP.AddAClass
 {
@@ -6,7 +7,32 @@ namespace OOP.AddAClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<Employee>()
+            {
+                new Employee {FirstName="Fatih", LastName="Cihan", Age=30},
+                new Employee {FirstName="Mehmet", LastName="Özer", Age=20},
+                new Employee {FirstName="Merve", LastName="Aslan", Age=25},
+            };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+            //Employee employee = new Employee()
+            //{
+            //    FirstName = "Fatih",
+            //    LastName = "Cihan",
+            //    Age = 30,
+            //};
+            //Employee employee2 = new Employee("Mehmet", "Özer", 30);
+            //Employee employee3 = new Employee("Merve", "Aslan", 20);
+
+            //list.Add(employee);
+            //list.Add(employee2);
+            //list.Add(employee3);
         }
     }
 }
