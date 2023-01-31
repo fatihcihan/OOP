8de3779d7ed528fd5c0cd98f1ce7dd665fa4b3bd
Fatih <fatihcihan666@gmail.com>
2023-02-01 01:18:40 +0300

    Gereksiz alanlar temizlendi

--- OOP.AddAClass/Program.cs
+++ OOP.AddAClass/Program.cs
@@ -10,7 +10,19 @@ namespace OOP.AddAClass
             var employee = new Employee();
             employee.Add(new Employee { FirstName = "Fatih", LastName = "Cihan", Age = 30 });
 
-            Console.ReadLine();            
+            employee.AddRange(
+                new Employee("Merve", "Aslan", 25),
+                new Employee("Murat", "Akay", 20),
+                new Employee("Elif", "Han", 33)
+                );
+
+            var list = employee.GetEmployees();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
