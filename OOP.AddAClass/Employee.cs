9d636a8b34900db8cb07d5d61633a254c6031bfe
Fatih <fatihcihan666@gmail.com>
2023-01-31 18:27:06 +0300

    Add project files.

---
+++ OOP.AddAClass/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.AddAClass
+{
+    internal class Employee
+    {
+        private string _firstName;
+
+        public string FirstName     // encapsulation
+        {
+            set { _firstName = value; }
+            get { return _firstName; }
+        }
+
+    }
+}
