c139fcac493d42cfe4c0d4e1dbadf56915e8e162
Fatih <fatihcihan666@gmail.com>
2023-02-01 14:50:13 +0300

    Square classi eklendi, Oblong class'indan inherit edildi.

---
+++ OOP.Inheritance/Square.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP.Inheritance
+{
+    public class Square : Oblong        // Multi-level inheritance. Base class'takileri de miras olarak aldi. Cok abartmamak lazim.
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Calculated by Square.");
+        }
+    }
+}
