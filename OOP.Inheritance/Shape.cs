bd797c52902dfd2ca9e8a10e495100ca49e14459
Fatih <fatihcihan666@gmail.com>
2023-02-01 14:14:30 +0300

    Inheritance icin gerekli ornekler yapildi

---
+++ OOP.Inheritance/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance
+{
+    public class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public void Draw()
+        {
+            Console.WriteLine("Base class draving task.");
+        }
+    }
+}
